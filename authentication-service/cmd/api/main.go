f7f74c8b1f620133e507b39bf935974779e75f30
Abhishek Kumar <abhirajabhi312@gmail.com>
2025-04-12 19:49:08 +0530

    add auth service to compose file

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -3,22 +3,88 @@ package main
 import (
 	"auth_service/data"
 	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 const webPort = "9090"
 
 type Config struct {
-	DB     *sql.DB
-	Models data.UserModel
+	m data.Models
 }
 
+var db *sql.DB
+var noOfConn int32
+
 func main() {
-	db, err := sql.Open("postgres", "")
-	if err != nil {
-		log.Fatal(err)
+
+	// slog.Info("Auth service main func", fmt.Sprintf("Auth"))
+	// Connect to db
+	conn := connectDB()
+	if conn != nil {
+		log.Panic("Can't connect to db")
 	}
-	defer db.Close()
+	models := data.NewModels(db)
+	// config:
+	cfg := Config{
+		m: models,
+	}
 
 	// models := data.NewModels(db)
+
+	// Server setup
+
+	// define the server
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: cfg.Routes(),
+	}
+
+	// start the server
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+// Db connection
+
+func openConn(uri string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", uri)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectDB() *sql.DB {
+	dsn := os.Getenv("DB_URI")
+
+	for {
+		connection, err := openConn(dsn)
+		if err != nil {
+			fmt.Println("DB not ready yet..")
+			noOfConn++
+		} else {
+			log.Println("DB connected...")
+			return connection
+		}
+		if noOfConn > 10 {
+			fmt.Println(err)
+			return nil
+		}
+
+		log.Println("Back of for 2 sec")
+		time.Sleep(2 * time.Second)
+		continue
+	}
 }
