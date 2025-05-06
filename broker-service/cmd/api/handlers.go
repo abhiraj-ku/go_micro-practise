2bce48f2806afd2838cc0476883f39c886c8d7eb
Abhishek Kumar <abhirajabhi312@gmail.com>
2025-05-07 00:13:03 +0530

    added logger-service to broker submission to handle log functionality

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -13,11 +13,17 @@ import (
 type RequestPayloadFormat struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -45,6 +51,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 
 	default:
 		app.ErrorJSON(w, errors.New("unknown action"))
@@ -126,4 +134,40 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	payload.Data = jsonFromAuth.Data
 
 	app.WriteJson(w, http.StatusAccepted, payload)
+}
+
+// LogItem functions calls the logger microservice and logs the items
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+	logServURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.ErrorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		app.ErrorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.ErrorJSON(w, err)
+		return
+	}
+
+	var payload jsonResponse
+
+	payload.Error = false
+	payload.Message = "logged"
+
+	app.WriteJson(w, http.StatusAccepted, payload)
+
 }
